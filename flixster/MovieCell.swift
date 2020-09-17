d55f1bf54b21f717c4d9e605fc1f31210cda3fb2
Richard <rwalker54@outlook.com>
2020-09-17 16:01:00 -0400

    Assignment 1 1.0

--- flixster/MovieCell.swift
+++ flixster/MovieCell.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
