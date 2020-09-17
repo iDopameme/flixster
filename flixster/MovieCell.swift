c4d937a608a29de43359f2e039341eedb5a2dc83
Richard <rwalker54@outlook.com>
2020-09-17 13:26:28 -0400

    New Filxster app for assignment 1

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by user180626 on 9/16/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
